687fac0ef8f27bd42b8cccf3612acf1bbce6fd91
huxly123 <huxlygerard5@gmail.com>
2021-09-29 16:02:27 +0530

    update

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,3 +1,22 @@
 //User Model:
 const mongoose=require('mongoose');
 
+const userSchema=new mongoose.Schema({
+    full_name:{type:String,required:true},
+    email:{type:String,required:true},
+    mobile:{type:Number,required:true},
+    password:{type:String,required:true},
+    gender:{type:String,required:true},
+    address:{type:String,required:true},
+    orders:[{type:mongoose.Schema.Types.ObjectId,ref:"product",required:true}],
+    wishlist:[{type:mongoose.Schema.Types.ObjectId,ref:"product",required:true}]
+},
+{
+    versionKey:false
+})
+
+const User=mongoose.model("user",userSchema)
+
+
+module.exports=User
+
