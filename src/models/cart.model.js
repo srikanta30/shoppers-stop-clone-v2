6ab74216bc002367e9c9a406dc52d617fc2624e1
huxly123 <huxlygerard5@gmail.com>
2021-09-29 16:15:11 +0530

    cart

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -1,1 +1,12 @@
 //Cart Model:
+
+
+const mongoose=require('mongoose');
+
+const cartSchema=new mongoose.model({
+    user:{type:mongoose.Schema.Types.ObjectId,ref:"user",required:true},
+    product:{type:mongoose.Schema.Types.ObjectId,ref:"product",required:true},
+    
+})
+
+const Cart=mongoose.model("cart",cartSchema);
