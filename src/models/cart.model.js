13e07de98d7dc0d16422d4d4014f0cb24463a1ba
huxly123 <huxlygerard5@gmail.com>
2021-09-29 16:16:19 +0530

    cart model done

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -6,7 +6,7 @@ const mongoose=require('mongoose');
 const cartSchema=new mongoose.model({
     user:{type:mongoose.Schema.Types.ObjectId,ref:"user",required:true},
     product:{type:mongoose.Schema.Types.ObjectId,ref:"product",required:true},
-    
+    quantity:{type:Number,required:true}
 })
 
 const Cart=mongoose.model("cart",cartSchema);
