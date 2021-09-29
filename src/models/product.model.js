b490ffb7e7606ee987e19e0117a21645be8ee62e
huxly123 <huxlygerard5@gmail.com>
2021-09-29 16:11:24 +0530

    product model done

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -13,9 +13,11 @@ const productSchema=new mongoose.model({
     size_2:{type:Number,required:true},
     size_3:{type:Number,required:true},
     size_4:{type:Number,required:true},
-    brand_name:{type:String,required:true},
-    brand_name:{type:String,required:true},
-    brand_name:{type:String,required:true},
-    brand_name:{type:String,required:true},
-    brand_name:{type:String,required:true},
+    color:{type:String,required:true},
+    image_1:{type:String,required:true},
+    image_2:{type:String,required:true},
+    image_3:{type:String,required:true},
+    image_4:{type:String,required:true},
 })
+
+const Product=mongoose.model("product",productSchema);
