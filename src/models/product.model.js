447a3d251536531f9fe1b9dfd497600ef490117b
huxly123 <huxlygerard5@gmail.com>
2021-09-29 16:09:38 +0530

    pro model

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,1 +1,21 @@
 //Product Model:
+
+const mongoose=require('mongoose');
+
+const productSchema=new mongoose.model({
+    product_name:{type:String,required:true},
+    description:{type:String,required:true},
+    price:{type:Number,required:true},
+    discount:{type:Number,required:true},
+    brand_name:{type:String,required:true},
+    rating:{type:Number,required:true},
+    size_1:{type:Number,required:true},
+    size_2:{type:Number,required:true},
+    size_3:{type:Number,required:true},
+    size_4:{type:Number,required:true},
+    brand_name:{type:String,required:true},
+    brand_name:{type:String,required:true},
+    brand_name:{type:String,required:true},
+    brand_name:{type:String,required:true},
+    brand_name:{type:String,required:true},
+})
