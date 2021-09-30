7562e90c212f0a73505682c74d8f50faffcb3393
Srikanta Banerjee <srikantabanerjee30@gmail.com>
2021-09-30 16:58:07 +0530

    models updated

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,13 +3,13 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    full_name : {type:String,required:true},
-    email : {type:String,required:true},
-    mobile : {type:Number,required:true},
-    password : {type:String,required:true},
-    gender : {type:String,required:true},
-    address : {type:String,required:true},
-    cart: [{ product : {type:mongoose.Schema.Types.ObjectId, ref:"product", required:true}, quantity: {Number,required:true}}],
+    username : {type:String,required:true},
+    useremail : {type:String,required:true},
+    usermobile : {type:Number,required:true},
+    userpassword : {type:String,required:true},
+    usergender : {type:String,required:true},
+    useraddress : {type:String,required:true},
+    cart: [{type:mongoose.Schema.Types.ObjectId, ref:"product", required:true}],
     orders : [{type:mongoose.Schema.Types.ObjectId,ref:"product",required:true}],
     wishlist : [{type:mongoose.Schema.Types.ObjectId,ref:"product",required:true}]
 },
