d66c31002f40d5cd939d97f2d359e1ba8f9a9b8d
Srikanta Banerjee <srikantabanerjee30@gmail.com>
2021-10-03 12:40:50 +0530

    updated server.js

--- src/server.js
+++ src/server.js
@@ -46,7 +46,7 @@ app.use("/wishlist", wishlistController);
 
 
 //For Server:
-app.listen(5000, async () => {
+app.listen(process.env.PORT || 5000, async () => {
 try {
  await connect();
  console.log("Listening On Port 5000...");
