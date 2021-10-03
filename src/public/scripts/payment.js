b5ac7d42d5d9d0ffb40f7ec46ca92dc12b066aca
Srikanta Banerjee <srikantabanerjee30@gmail.com>
2021-10-03 16:20:50 +0530

    updated payment and payment successful page

--- src/public/scripts/payment.js
+++ src/public/scripts/payment.js
@@ -46,78 +46,91 @@ window.addEventListener("load", async function(){
 
 paypal.Buttons({
 
-    // Sets up the transaction when a payment button is clicked
     createOrder: function(data, actions) {
       return actions.order.create({
         purchase_units: [{
           amount: {
-            value: document.getElementById('payableamount').value // Can reference variables or functions. Example: `value: document.getElementById('...').value`
+            value: document.getElementById('payableamount').value
           }
         }]
       });
     },
 
-    // Finalize the transaction after payer approval
+    
     onApprove: function(data, actions) {
       return actions.order.capture().then(function(orderData) {
-        // Successful capture! For dev/demo purposes:
+   
             console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
             var transaction = orderData.purchase_units[0].payments.captures[0];
-            // alert('Transaction '+ transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
+            console.log(transaction);
+           
             window.location.href = 'processing';
-            // actions.redirect('processing');
-
-        // When ready to go live, remove the alert and show a success message within this page. For example:
-        // let element = document.getElementById('paypal');
-        // element.innerHTML = '';
-        // element.innerHTML = '<h3>Thank you for your payment!</h3>';
-        // Or go to another URL:  actions.redirect('processing');
+            
       });
     }
   }).render('#paypal');
 
-// window.addEventListener("load", function(){
-//     let applypromo = document.getElementById("applyapomo");
-//     applypromo.addEventListener("click", function(){
-//         let promo = document.getElementById("promo").value;
-//         if (promo == "masai30"){
-//             alert("Discount Applied!");
-//             let carttotal = localStorage.getItem("carttotal");
-//             let discount = Math.round(carttotal * .30)
-//             carttotal = Math.round(carttotal * .70);
-//             let total = document.getElementById("total");
-//             total.innerHTML = `Payable Amount: ₹${carttotal}`;
-//             let coupon = document.getElementById("coupon");
-//             coupon.textContent = "30%";
-//             let payableamount = document.getElementById("payableamount");
-//             payableamount.textContent = `₹${carttotal}`;
-//             let totaldiscount = document.getElementById("totaldiscount");
-//             totaldiscount.textContent = `₹${discount}`;
+window.addEventListener("load", function(){
+    let applypromo = document.getElementById("applyapomo");
+    applypromo.addEventListener("click", function(){
+        let promo = document.getElementById("promo").value;
+        if (promo == "masai30"){
+            alert("Discount Applied!");
 
 
-//         }
-//         else {
-//             alert("Coupon Code Not Valid!");
-//         }
+
+            let res = await fetch (`https://shoppers-stop-com.herokuapp.com/user/${currentuser}`);
+            let data = await res.json();
+            let cart = data.item[0].cart;
+            let carttotal = 0;
         
-//     })
-// })
+            cart.forEach(async (pro) => {
+                pro = Number(pro);
+                let res = await fetch(`https://shoppers-stop-com.herokuapp.com/product/view/${pro}`);
+                let data = await res.json();
+                let product = data.item[0];
+                carttotal = carttotal + Math.round(product.price - (product.price * product.discount / 100));
+                
+                let discount = Math.round(carttotal * .30)
+            carttotal = Math.round(carttotal * .70);
+            let total = document.getElementById("total");
+            total.innerHTML = `Payable Amount: ₹${carttotal}`;
+            let coupon = document.getElementById("coupon");
+            coupon.textContent = "30%";
+            let payableamount = document.getElementById("payableamount");
+            payableamount.textContent = `₹${carttotal}`;
+            let totaldiscount = document.getElementById("totaldiscount");
+            totaldiscount.textContent = `₹${discount}`;
+            })
+
+
+
+            
+
+
+        }
+        else {
+            alert("Coupon Code Not Valid!");
+        }
+        
+    })
+})
 
 
 
 
-// window.addEventListener("load", function(){
-//     let proceedtocheckout = document.getElementById("proceedtocheckout");
-//     proceedtocheckout.addEventListener("click", function(){
-//         let cvv = document.getElementById("cvv").value;
-//         let cardnumber = document.getElementById("cardnumber").value;
-//         if (cardnumber == "" || cvv == ""){
-//             alert("Invalid Card Details");
-//         }
-//         else {
-//             window.location.href = "orderprocessing";
-//         }
-//     })
+window.addEventListener("load", function(){
+    let proceedtocheckout = document.getElementById("proceedtocheckout");
+    proceedtocheckout.addEventListener("click", function(){
+        let cvv = document.getElementById("cvv").value;
+        let cardnumber = document.getElementById("cardnumber").value;
+        if (cardnumber == "" || cvv == ""){
+            alert("Invalid Card Details!");
+        }
+        else {
+            window.location.href = "orderprocessing";
+        }
+    })
 
-// })
+})
 
--- src/public/scripts/successful.js
+++ src/public/scripts/successful.js
@@ -1,2 +1,41 @@
 //Order Successful Page:
 
+window.addEventListener('load', async() => {
+
+    if (localStorage.getItem("currentuser") === null){
+        alert("Please Login To Continue!");
+        window.location.href = "signin"
+    }
+
+    else {
+    let currentuser = localStorage.getItem("currentuser");
+    let res = await fetch (`https://shoppers-stop-com.herokuapp.com/user/${currentuser}`);
+    let data = await res.json();
+    let cart = data.item[0].cart;
+    let orders = data.item[0].orders;
+    let neworders = [...cart, ...orders];
+    let emptycart = [];
+
+    let sendres = await fetch(`https://shoppers-stop-com.herokuapp.com/user/${currentuser}`, {
+    method: 'PATCH',
+    body: JSON.stringify({
+    orders: neworders,
+    cart: emptycart
+    }),
+    headers: {
+   'Content-type': 'application/json; charset=UTF-8'
+   }
+  })
+
+  let resdata = await sendres.json();
+  return resdata;
+
+    }
+})
+
+let logout = document.getElementById('logout');
+logout.onclick = () => {
+    localStorage.setItem("currentuser", null);
+    window.location.href = '/';
+}
+
